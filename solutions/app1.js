f1f02bd287aa7407910a7b816ab7ed7482e83c28
vargapeter66 <varga.peter.mobil@gmail.com>
2021-11-18 15:33:54 +0100

    test ok

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,11 @@
+const converterPromise = (array) => new Promise((resolve, reject) => {
+  if (array.every(item => typeof item === "string")) {
+  const arr1 = array.map(item => item.toUpperCase());    
+  resolve(arr1);
+}
+else {  
+  reject("Error: Not all elements are string type!");
+}
+});
 
-
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,11 @@
+const sorterPromise = (array) => new Promise((resolve, reject) => {
+  if (array.every(item => typeof item === "string")) {
+    const arr2 = array = array.sort();
+    resolve(arr2)
+  }
+  else {    
+    reject("Error: Not all elements are string type!");
+  }
+});
 
-
-
-
-
-
-// export default sorterPromise;
+export default sorterPromise;
